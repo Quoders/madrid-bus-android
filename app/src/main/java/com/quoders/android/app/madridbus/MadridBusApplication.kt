dc05a42ef897c946281768e63710f4d21290da28
David Guerrero <david.guerrero@quoders.com>
2019-12-21 18:23:46 +0000

    Add device location feature

--- app/src/main/java/com/quoders/android/app/madridbus/MadridBusApplication.kt
+++ app/src/main/java/com/quoders/android/app/madridbus/MadridBusApplication.kt
@@ -3,11 +3,21 @@ package com.quoders.android.app.madridbus
 import com.quoders.android.app.madridbus.di.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class MadridBusApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerApplicationComponent.builder().application(this).build()
     }
 
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
 }
 
--- app/src/main/java/com/quoders/android/app/madridbus/ui/around/AroundMeFragment.kt
+++ app/src/main/java/com/quoders/android/app/madridbus/ui/around/AroundMeFragment.kt
@@ -1,22 +1,36 @@
 package com.quoders.android.app.madridbus.ui.around
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 import com.quoders.android.app.madridbus.R
+import timber.log.Timber
+
 
 class AroundMeFragment : Fragment(), OnMapReadyCallback {
 
-    companion object {
-        fun newInstance() = AroundMeFragment()
-    }
-
     private lateinit var viewModel: AroundMeViewModel
+
+    private var locationPermissionGranted: Boolean = false
+    private var googleMap: GoogleMap? = null
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var lastKnownLocation: Location? = null
+    private val defaultLocation = LatLng(40.416819, -3.703792)
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,17 +42,118 @@ class AroundMeFragment : Fragment(), OnMapReadyCallback {
     }
 
     private fun initialiseMapFragment() {
-        val mapFragment = childFragmentManager.findFragmentById(R.id.mapAround) as SupportMapFragment
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapAround) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        getLocationPermission()
     }
 
-    override fun onMapReady(googleMap: GoogleMap?) {
-        googleMap?.isIndoorEnabled
+    override fun onMapReady(map: GoogleMap?) {
+        googleMap = map
+        googleMap?.isMyLocationEnabled = true
+
     }
 
+    private fun getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationPermissionGranted = true
+            getDeviceLocation()
+            updateLocationUI()
+        } else {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    private fun updateLocationUI() {
+        try {
+            if (locationPermissionGranted) {
+                googleMap?.isMyLocationEnabled = true
+                googleMap?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                googleMap?.isMyLocationEnabled = false
+                googleMap?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Timber.e(e)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        locationPermissionGranted = false
+
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            if (grantResults.isNotEmpty()
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                locationPermissionGranted = true
+                updateLocationUI()
+                getDeviceLocation()
+            }
+        }
+    }
+
+    private fun getDeviceLocation() {
+
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        lastKnownLocation = task.result
+                        googleMap?.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(
+                                LatLng(
+                                    lastKnownLocation?.latitude!!,
+                                    lastKnownLocation?.longitude!!
+                                ),
+                                DEFAULT_MAP_ZOOM
+                            )
+                        )
+                    } else {
+                        Timber.i("Unable to get device location")
+                        googleMap?.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(defaultLocation, DEFAULT_MAP_ZOOM)
+                        )
+                        googleMap?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+
+        } catch (exception: Exception) {
+            Timber.e(exception)
+        }
+    }
+
+    companion object {
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0x0001
+        private const val DEFAULT_MAP_ZOOM = 18f
+
+        fun newInstance() = AroundMeFragment()
+    }
 }
